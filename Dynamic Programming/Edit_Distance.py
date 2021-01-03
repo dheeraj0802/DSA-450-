cd1738ce8262dd16b067e8c13d39a3f0a03abc02
Dheeraj Khatri <50199862+dheeraj0802@users.noreply.github.com>
2021-01-03 22:51:25 +0530

    Update Edit_Distance.py

--- Dynamic Programming/Edit_Distance.py
+++ Dynamic Programming/Edit_Distance.py
@@ -3,14 +3,14 @@ class Solution:
 		# Code here
 		dp=[[0 for i in range(len(t)+1)] for j in range(len(s)+1) ]
         
-        for i in range(len(s)+1):
-            for j in range(len(t)+1):
-                if i==0:
-                    dp[i][j]=j
-                elif j==0:
-                    dp[i][j]=i
-                elif s[i-1]==t[j-1]:
-                    dp[i][j]=dp[i-1][j-1]
-                else:
-                    dp[i][j]=1+min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
-        return dp[-1][-1]            
+        	for i in range(len(s)+1):
+            		for j in range(len(t)+1):
+                		if i==0:
+                    			dp[i][j]=j
+                		elif j==0:
+                    			dp[i][j]=i
+                		elif s[i-1]==t[j-1]:
+                    			dp[i][j]=dp[i-1][j-1]
+                		else:
+                    			dp[i][j]=1+min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
+        	return dp[-1][-1]            
